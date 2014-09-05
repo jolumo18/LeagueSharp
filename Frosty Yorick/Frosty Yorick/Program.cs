a2e58dc78b12996b5750f58b497f3e2ddc217376
newchild <fabianbrand98@gmx.de>
2014-09-05 22:26:57 +0200

    Yorick Denies to Cast E fixed. No Harrass

--- Frosty Yorick/Frosty Yorick/Program.cs
+++ Frosty Yorick/Frosty Yorick/Program.cs
@@ -14,7 +14,7 @@ namespace Frosty_Yorick
     {
         public static string ChampName = "Yorick";
         public static Orbwalking.Orbwalker Orbwalker;
-        public static Obj_AI_Base Player = ObjectManager.Player; // Instead of typing ObjectManager.Player you can just type Player
+        public static Obj_AI_Base Player = ObjectManager.Player;
         public static Spell Q, W, E, R;
 
         public static Menu YorickWrapper;
@@ -25,11 +25,12 @@ namespace Frosty_Yorick
 
         static void Game_OnGameLoad(EventArgs args)
         {
+            Game.PrintChat("Frosty " + ChampName + " by newchild");
             if (Player.BaseSkinName != ChampName) return;
 
             Q = new Spell(SpellSlot.Q, 0);
             W = new Spell(SpellSlot.W, 600);
-            E = new Spell(SpellSlot.E, 650);
+            E = new Spell(SpellSlot.E, 500);
             R = new Spell(SpellSlot.R, 900);
             W.SetSkillshot(0.6f, 0f, 20f, false, SkillshotType.SkillshotCircle);
 
@@ -43,9 +44,7 @@ namespace Frosty_Yorick
             YorickWrapper.AddSubMenu(ts);
             YorickWrapper.AddSubMenu(new Menu("Combo", "Combo"));
             YorickWrapper.AddSubMenu(new Menu("Harrass", "Harrass"));
-            YorickWrapper.SubMenu("Harrass").AddItem(new MenuItem("HarrassActive", "Harrass!").SetValue(new KeyBind("X".ToCharArray()[0], KeyBindType.Press)));
-            YorickWrapper.SubMenu("Harrass").AddItem(new MenuItem("UseWH", "Use W in Harrass").SetValue(true));
-            YorickWrapper.SubMenu("Harrass").AddItem(new MenuItem("UseEH", "Use E in Harrass").SetValue(true));
+            YorickWrapper.SubMenu("Harrass").AddItem(new MenuItem("HarrassActive", "Harrass!").SetValue(new KeyBind(226, KeyBindType.Toggle)));
             YorickWrapper.SubMenu("Combo").AddItem(new MenuItem("useR", "Use R at Health:").SetValue(new Slider(5, 0, 100)));
             YorickWrapper.SubMenu("Combo").AddItem(new MenuItem("ComboActive", "Combo!").SetValue(new KeyBind(32, KeyBindType.Press)));
 
@@ -73,42 +72,42 @@ namespace Frosty_Yorick
 
         static void Drawing_OnDraw(EventArgs args)
         {
-            Drawing.DrawCircle(Player.Position, E.Range, Color.AliceBlue);
-            Drawing.DrawCircle(Player.Position, E.Range, Color.Black);
         }
 
         public static void Combo()
         {
-            var target = SimpleTs.GetTarget(W.Range, SimpleTs.DamageType.Physical);
+            var target = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical);
             if (target == null) return;
+            if (target.Team != Player.Team)
+            {
+                if (target.IsValidTarget(Player.AttackRange) && Q.IsReady())
+                {
+                    Q.Cast();
 
-            if (target.IsValidTarget(Q.Range) && Q.IsReady())
-            {
-                Q.Cast();
-
-            }
-            if (target.IsValidTarget(W.Range) && W.IsReady())
-            {
-                W.Cast(target, YorickWrapper.Item("NFE").GetValue<bool>());
-            }
-            if (target.IsValidTarget(E.Range) && E.IsReady())
-            {
-                E.Cast(target, YorickWrapper.Item("NFE").GetValue<bool>());
-            }
-            if (YorickWrapper.Item("useR").GetValue<Slider>().Value < ((Player.Health/Player.MaxHealth)*100) && R.IsReady())
-            {
-                R.Cast(Player, YorickWrapper.Item("NFE").GetValue<bool>());
+                }
+                if (target.IsValidTarget(W.Range) && W.IsReady())
+                {
+                    W.Cast(target, YorickWrapper.Item("NFE").GetValue<bool>());
+                }
+                if (target.IsValidTarget(E.Range) && E.IsReady())
+                {
+                    E.Cast(target.ServerPosition, YorickWrapper.Item("NFE").GetValue<bool>());
+                }
+                if (YorickWrapper.Item("useR").GetValue<Slider>().Value < ((Player.Health / Player.MaxHealth) * 100) && R.IsReady())
+                {
+                    R.Cast(Player, YorickWrapper.Item("NFE").GetValue<bool>());
+                }
             }
         }
         public static void Harrass()
         {
-            var target = SimpleTs.GetTarget(W.Range, SimpleTs.DamageType.Physical);
-            if (YorickWrapper.Item("useEH").GetValue<bool>() && E.IsReady() && target.IsValidTarget(E.Range))
+            var target = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical);
+            if ( E.IsReady() && target.IsValidTarget(E.Range))
             {
                 
-                E.Cast(target,YorickWrapper.Item("NFE").GetValue<bool>());
+                E.Cast(target.ServerPosition,YorickWrapper.Item("NFE").GetValue<bool>());
             }
-            if (YorickWrapper.Item("useWH").GetValue<bool>() && W.IsReady() && target.IsValidTarget(W.Range))
+            if (W.IsReady() && target.IsValidTarget(W.Range))
             {
 
                 W.Cast(target, YorickWrapper.Item("NFE").GetValue<bool>());
