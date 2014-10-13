bdc680e58c5940c26f9378488feb371053f9fbf6
newchild <fabianbrand98@gmx.de>
2014-10-13 20:40:36 +0200

    Update Program.cs

--- Frosty Twitch/Frosty Twitch/Program.cs
+++ Frosty Twitch/Frosty Twitch/Program.cs
@@ -143,7 +143,7 @@ namespace Frosty_Yorick
                         .Where(
                             hero =>
                                 hero.IsValidTarget(E.Range) &&
-                                (E.GetDamage(hero)) - 15 > hero.Health))
+                                (E.GetDamage(hero)) > hero.Health))
                         E.Cast();
                 }
                 if (target.IsValidTarget((R.Range - 50)) && R.IsReady())
